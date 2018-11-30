b0a792dc9e7352ba632941fab7d21b853b56d341
Aanand Shekhar Roy <aanandshekharroy@users.noreply.github.com>
2018-11-30 11:12:56 +0530

    style: UI Improvements in detail activity

--- app/src/main/java/com/example/theseus/githubtrend/ui/detail/RepoDetailActivity.kt
+++ app/src/main/java/com/example/theseus/githubtrend/ui/detail/RepoDetailActivity.kt
@@ -1,11 +1,18 @@
 package com.example.theseus.githubtrend.ui.detail
 
+import android.app.PendingIntent.getActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import com.bumptech.glide.Glide
 import com.example.theseus.githubtrend.R
 import com.example.theseus.githubtrend.data.api.model.TrendingAPIResponse.RepoItem
 import com.example.theseus.githubtrend.ui.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_detail.*
+import android.content.pm.PackageManager
+import android.util.Log
+import org.jetbrains.anko.toast
+
 
 class RepoDetailActivity : BaseActivity(), IRepoDetailView {
     lateinit var mRepo: RepoItem
@@ -24,6 +31,18 @@ class RepoDetailActivity : BaseActivity(), IRepoDetailView {
         repo_description.text = mRepo.description
         supportActionBar?.let {
             it.title = mRepo.name
+        }
+        github_url.setOnClickListener {
+            openRepoLinkInBrowser()
+        }
+    }
+
+    private fun openRepoLinkInBrowser() {
+        val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse(mRepo.owner.htmlUrl))
+        if (webIntent.resolveActivity(packageManager) != null) {
+            startActivity(webIntent)
+        } else {
+            toast(R.string.cant_launch_url)
         }
     }
 }
